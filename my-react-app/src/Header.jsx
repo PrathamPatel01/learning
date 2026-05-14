f2d3d9322cc21bfa679a10c83922404839a633ea
PrathamPatel01 <pnpatel3ac@gmail.com>
2026-05-14 23:57:08 +0200

    updatee

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -5,7 +5,7 @@
 function Header(){
 
     return(<header>
-        <h1>My website App</h1>
+        <h1>My website Apppp</h1>
         <nav>
           <ul>    
             <li><a href="#">Home</a></li>
