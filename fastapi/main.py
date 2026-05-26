a810e599691eb52e5e1f8c98d38d0ad919b7ca2d
PrathamPatel01 <pnpatel3ac@gmail.com>
2026-05-27 01:25:05 +0200

    postgresSql

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,42 +1,99 @@
-# from fastapi import FastAPI
+# # from fastapi import FastAPI
 
+# # app = FastAPI()
+
+# # @app.get("/get")
+# # def home():
+# #     return {"message": "working"}
+
+
+ 
+# from fastapi import FastAPI, HTTPException
+# from pydantic import BaseModel
+ 
 # app = FastAPI()
 
-# @app.get("/get")
+# todos = []
+
+# class Todo(BaseModel):
+#     title: str
+#     completed: bool = False
+
+
+# @app.get("/")
 # def home():
-#     return {"message": "working"}
+#     return {"message": "FastAPI working"}
 
+# @app.post("/todos")
+# def create_todo(todo:Todo):
+#     todos.append(todo)
+#     print(todos)
+#     return todo
 
- 
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
- 
+# @app.get("/todos/{todo_id}")
+# def get_todo(todo_id: int):
+#     if todo_id < 0 or todo_id >= len(todos):
+
+#         raise HTTPException(
+#             status = 404,
+#             detail ="Todo not found"
+#         )
+    
+#     return todos[todo_id]
+
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from database import SessionLocal, engine
+from models import Base, Todo
+from schemas import TodoCreate
+
 app = FastAPI()
 
-todos = []
+Base.metadata.create_all(bind=engine)
 
-class Todo(BaseModel):
-    title: str
-    completed: bool = False
+
+def get_db():
+
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
 
 
 @app.get("/")
 def home():
-    return {"message": "FastAPI working"}
+    return {"message": "FastAPI PostgreSQL working"}
+
 
 @app.post("/todos")
-def create_todo(todo:Todo):
-    todos.append(todo)
-    print(todos)
-    return todo
+def create_todo(
+    todo: TodoCreate,
+    db: Session = Depends(get_db)
+):
 
-@app.get("/todos/{todo_id}")
-def get_todo(todo_id: int):
-    if todo_id < 0 or todo_id >= len(todos):
+    new_todo = Todo(
+        title=todo.title,
+        completed=todo.completed
+    )
 
-        raise HTTPException(
-            status = 404,
-            detail ="Todo not found"
-        )
-    
-    return todos[todo_id]
+    db.add(new_todo)
+
+    db.commit()
+
+    db.refresh(new_todo)
+
+    return new_todo
+
+
+@app.get("/todos")
+def get_todos(
+    db: Session = Depends(get_db)
+):
+
+    todos = db.query(Todo).all()
+
+    return todos
